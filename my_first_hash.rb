be89e85acaedddcc7674b20fb0bd7dea9a54d2c5
Erica Schimmel <ericaschimmel01@gmail.com>
2020-01-22 05:43:08 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,7 +1,7 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
 
-  my_hash = {
+  return my_hash = {
     game: "Donkey Kong Country 2",
     release_date: "November 21, 1995"
   }
